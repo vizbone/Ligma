ee7887bd0d5177476b8bbc79dc2c8b0a91c123c4
Koh Guan Zeh <kohguanzeh960@gmail.com>
2018-11-13 22:21:13 +0800

    Updated Mana System
    Updated the Mana System

--- Atlas Ligma/Assets/Scripts/ManaSystem.cs
+++ Atlas Ligma/Assets/Scripts/ManaSystem.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class ManaSystem : MonoBehaviour
 {
-
-	public int maxMana;
+	public int maxMana; //Amt of mana players will have to reach to win the game. MAY differ for each level
 	public int currentMana;
-	public int manaRegenPerTick;
-	public int manaRegenRate;
 
-	bool cLock;
-	float time;
+	[SerializeField] private int startingMana; //Amt of mana players will start with. Will differ for each level
 
-	void Start ()
+	private void Start()
+	{
+		currentMana = startingMana;
+	}
+
+	void Update()
+	{
+		//Players win the game once their Current Mana is >= Max Mana
+		if (currentMana > maxMana) print("Victory");
+		else if (currentMana <= 0) print("Defeated");
+	}
+
+	//Subtracts mana from bank
+	public void ManaMinus (int amount) 
+	{
+		currentMana -= amount;
+	}
+
+	//Adds mana from bank
+	public void ManaAdd (int amount)
+	{
+		currentMana += amount;
+	}
+
+	//public int manaRegenPerTick;
+	//public int manaRegenRate;
+
+	//bool cLock;
+	//float time;
+
+	//Old Mana System
+	/*void Start ()
 	{
 		if (manaRegenRate <= 0) { manaRegenRate = 1; }
 		time = 1 / manaRegenRate;
 		cLock = false;
-		currentMana = maxMana; 
+		currentMana = maxMana;
 	}
 
 	void Update ()
@@ -33,18 +60,5 @@ public class ManaSystem : MonoBehaviour
 		if (currentMana > maxMana) { currentMana = maxMana; }
 		yield return new WaitForSeconds (time);
 		cLock = false;
-	}
-
-	//minuses mana from bank
-	public void ManaMinus (int amount) 
-	{
-		currentMana -= amount;
-	}
-
-	//adds mana from bank
-	public void ManaAdd (int amount)
-	{
-		currentMana += amount;
-		if (currentMana > maxMana) { currentMana = maxMana; }
-	}
+	}*/
 }
