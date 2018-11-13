895feabbb4fb1c505633f007b6253cbaadbc47ff
viz <vizbone@users.noreply.github.com>
2018-11-13 23:55:16 +0800

    Revert "Merge branch 'master' of https://github.com/vizbone/Ligma"
    This reverts commit 59e9a65eae4b05732b36642630ffe1c5e79ae378, reversing
    changes made to 24871b9320036d778d8339410822eadf6e7fed75.

--- Atlas Ligma/Assets/Scripts/ManaSystem.cs
+++ Atlas Ligma/Assets/Scripts/ManaSystem.cs
@@ -4,48 +4,21 @@ using UnityEngine;
 
 public class ManaSystem : MonoBehaviour
 {
-	public int maxMana; //Amt of mana players will have to reach to win the game. MAY differ for each level
+
+	public int maxMana;
 	public int currentMana;
+	public int manaRegenPerTick;
+	public int manaRegenRate;
 
-	[SerializeField] private int startingMana; //Amt of mana players will start with. Will differ for each level
+	bool cLock;
+	float time;
 
-	private void Start()
-	{
-		currentMana = startingMana;
-	}
-
-	void Update()
-	{
-		//Players win the game once their Current Mana is >= Max Mana
-		if (currentMana > maxMana) print("Victory");
-		else if (currentMana <= 0) print("Defeated");
-	}
-
-	//Subtracts mana from bank
-	public void ManaMinus (int amount) 
-	{
-		currentMana -= amount;
-	}
-
-	//Adds mana from bank
-	public void ManaAdd (int amount)
-	{
-		currentMana += amount;
-	}
-
-	//public int manaRegenPerTick;
-	//public int manaRegenRate;
-
-	//bool cLock;
-	//float time;
-
-	//Old Mana System
-	/*void Start ()
+	void Start ()
 	{
 		if (manaRegenRate <= 0) { manaRegenRate = 1; }
 		time = 1 / manaRegenRate;
 		cLock = false;
-		currentMana = maxMana;
+		currentMana = maxMana; 
 	}
 
 	void Update ()
@@ -60,5 +33,18 @@ public class ManaSystem : MonoBehaviour
 		if (currentMana > maxMana) { currentMana = maxMana; }
 		yield return new WaitForSeconds (time);
 		cLock = false;
-	}*/
+	}
+
+	//minuses mana from bank
+	public void ManaMinus (int amount) 
+	{
+		currentMana -= amount;
+	}
+
+	//adds mana from bank
+	public void ManaAdd (int amount)
+	{
+		currentMana += amount;
+		if (currentMana > maxMana) { currentMana = maxMana; }
+	}
 }
