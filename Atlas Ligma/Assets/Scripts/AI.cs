25c592ed6d7b208dfa52851c081c2e0fcc89f781
viz <vizbone@users.noreply.github.com>
2018-11-13 14:16:30 +0800

    AI Updated
    new ai boise

---
+++ Atlas Ligma/Assets/Scripts/AI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+
+	public int currentDestination;
+	NavMeshAgent agent;
+	AIMovement ai;
+
+	void Start () 
+	{
+		ai = FindObjectOfType<AIMovement> ();
+		agent = GetComponent<NavMeshAgent> ();
+		currentDestination = 0;
+		agent.SetDestination (ai.points[0].transform.position);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Waypoint") ai.NextPoint (agent, this);
+	}
+}
---
+++ Atlas Ligma/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour {
+
+	public Transform[] points;
+	
+	public void NextPoint (NavMeshAgent ai, AI script) 
+	{
+		script.currentDestination++;
+		ai.SetDestination (points[script.currentDestination].position); 
+	}
+}
