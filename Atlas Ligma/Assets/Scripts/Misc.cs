87b7735f2c8d6926a7445c6393213b38dfafd54d
viz <vizbone@users.noreply.github.com>
2018-11-23 12:44:29 +0800

    GRENADA FOR THE MOTHERLAND

---
+++ Atlas Ligma/Assets/Scripts/Misc.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misc : MonoBehaviour {
+	void Start ()
+	{
+		Physics.IgnoreLayerCollision (11, 11);
+	}
+}
