7fc57467b83e466c92c8f2d1996be912b247c76b
Arcardial <44972049+Arcardial@users.noreply.github.com>
2018-12-23 01:33:06 +0800

    AudioScripts
    Audio stuff (70%) left with just adding which audio is needed

---
+++ Atlas Ligma/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public Audios[] sfx;
+
+	private void Awake()
+	{
+		foreach(Audios x in sfx)
+		{
+			x.source = gameObject.AddComponent<AudioSource>();      //x.source of class Audios = the gameobject's AudioSource
+			x.source.clip = x.audioClip;                            //x.source of class Audios = the gameobject's AudioClip
+			x.source.volume = x.volume;                             //x.source of class Audios = the gameobject's Volume
+			x.source.pitch = x.pitch;                               //x.source of class Audios = the gameobject's Pitch
+		}
+	}
+	public void AudioToPlay(string name)
+	{
+		Audios x = Array.Find(sfx, Audios => Audios.name == name);	//Searching of the same name in the array
+		if(x == null)
+		{
+			Debug.Log("Sound" + name + "Not Found");
+			return;
+		}
+		x.source.Play();											//Playing the audio clip of that name
+
+	}
+}
---
+++ Atlas Ligma/Assets/Scripts/Audios.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Audios
+{
+	public string name;				//name of the audio
+	public AudioClip audioClip;			//the audio clip itself;
+
+	[Range(0f,1f)]					//min and max of the volume level
+	public float volume;			//volume of the clip
+		
+	[Range(0.1f, 3f)]				//min and max of the pitch
+	public float pitch;				//oitch of the clip
+
+	[HideInInspector]
+	public AudioSource source;		//the value to store the audio 
+	
+}
